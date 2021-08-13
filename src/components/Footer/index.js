a9a250dcf19685c7d4aac8309866d274a3e89ee9
sagar_2001 <sagarshaw2001@gmail.com>
2021-08-13 21:37:00 +0530

    changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -23,20 +23,20 @@ const Footer = () => {
         <FooterLinksContainer>
           <FooterLinksWrapper>
             <FooterLinkItems>
-              <FooterLinkTitle>About Us</FooterLinkTitle>
-              <FooterLink to="/signin">How it works</FooterLink>
-              <FooterLink to="/signin">Testimonials</FooterLink>
-              <FooterLink to="/signin">Careers</FooterLink>
-              <FooterLink to="/signin">Terms of Service</FooterLink>
-              <FooterLink to="/signin">Investors</FooterLink>
+              <FooterLinkTitle>Product</FooterLinkTitle>
+              <FooterLink to="/signin">Features</FooterLink>
+              <FooterLink to="/signin">Tablet Menu</FooterLink>
+              <FooterLink to="/signin">Dine-in QR Menu</FooterLink>
+              <FooterLink to="/signin">Delivery & Pick-Up Menu</FooterLink>
+              {/* <FooterLink to="/signin">Investors</FooterLink> */}
             </FooterLinkItems>
             <FooterLinkItems>
-              <FooterLinkTitle>About Us</FooterLinkTitle>
-              <FooterLink to="/signin">How it works</FooterLink>
-              <FooterLink to="/signin">Testimonials</FooterLink>
-              <FooterLink to="/signin">Careers</FooterLink>
+              <FooterLinkTitle>Business Types</FooterLinkTitle>
+              <FooterLink to="/signin">Cafes & Bakery</FooterLink>
+              <FooterLink to="/signin">Coffee Shop</FooterLink>
+              {/* <FooterLink to="/signin">Careers</FooterLink>
               <FooterLink to="/signin">Terms of Service</FooterLink>
-              <FooterLink to="/signin">Investors</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink> */}
             </FooterLinkItems>
           </FooterLinksWrapper>
           <FooterLinksWrapper>
